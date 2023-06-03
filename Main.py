e5a1c97d1eedb2a737bf46ddc50acb3adb58421d
ZefraAlseif <carlosotero9819@gmail.com>
2023-06-03 18:10:27 -0400

    Improved the check to see if the game has ended. Also added a function to get the score of th winning player.

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
 
-import random
-
-from Logic import Logic
-
 
 class main:
     
    
     def __init__(self) -> None:
         pass
-   
+
+test = [["Player1"],["Player2",1],["Player3",3,4]]
+
+if test[1][-1] == test[1][0]:
+    print('Works')
+else:
+    print("haha")
